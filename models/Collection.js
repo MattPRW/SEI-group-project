c1b2d477393587d8ec27ef517502f507933d662d
MPRW <matt@pamr.co.uk>
2019-10-24 17:12:16 +0100

    collection model created

---
+++ models/Collection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose') 
+
+const collectionSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  album: { type: mongoose.Schema.ObjectId, ref: 'Album', required: true },
+  rating: { type: Number }
+})
+
+module.exports = mongoose.model('Collection', collectionSchema)
