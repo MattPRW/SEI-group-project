0e0f26a2fb08e1fde8c5a9860fb8458fa6565a0c
grudinsky <g.rudzianskas@gmail.com>
2019-10-25 12:17:59 +0100

    album search function works, the rest of album controller outstanding

--- src/app.js
+++ src/app.js
@@ -2,37 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import axios from 'axios'
+import './normalize.css'
+import './skeleton.css'
 
 
-onst App = () => (
+import AlbumSearch from './components/albums/AlbumSearch'
+
+const App = () => (
   <BrowserRouter>
     <main>
       <Switch>
-        <Route path="/albumSearch" component={Profile} />
+        <Route path="/" component={AlbumSearch} />
       </Switch>
     </main>
   </BrowserRouter>
 )
-
-
-class AlbumSearch extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      data: null
-    }
-  }
-  componentDidMount() {
-  }
-  render() {
-    // if (!this.state.data) return null
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(
   <App />,
