9b317358fe530e6a5066240640964ad0d4d03a77
MPRW <matt@pamr.co.uk>
2019-10-28 16:46:09 +0000

    tiny-profile-pics

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-// import axios from 'axios'
 import 'bulma'
 import './normalize.css'
 import './skeleton.css'
@@ -15,7 +14,6 @@ import Dashboard from './components/auth/Dashboard'
 import Home from './components/common/Home'
 import AlbumSearch from './components/albums/AlbumSearch'
 import AlbumsIndex from './components/albums/AlbumsIndex'
-
 
 const App = () => (
   <BrowserRouter>
