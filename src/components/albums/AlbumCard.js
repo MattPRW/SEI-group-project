22e313522cad19b330d84de4812ab4dc87838842
MPRW <matt@pamr.co.uk>
2019-10-29 15:00:34 +0000

    a bit more styling

--- src/components/albums/AlbumCard.js
+++ src/components/albums/AlbumCard.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 const AlbumCard = ({ title, artist, coverImage, id, handleAddAlbum }) => (
 
-  <div className="column-is-one-third" value={id} key={id} >
-    <div>
-      <h5 className="title-size">{title}</h5>
-      <h6>{`Artist: ${artist.name}`}</h6>
-    </div>
+  <div className="column-is-one-third album-card" value={id} key={id} >
     <div className="image">
       <img src={coverImage}></img>
     </div>
-    {handleAddAlbum && (<div onClick={handleAddAlbum} className="button" id={id}>
+    <div>
+      <h5 className="title-size">{title}</h5>
+      <h6>{`${artist.name}`}</h6>
+    </div>
+    {handleAddAlbum && (<div onClick={handleAddAlbum} className="button add-button" id={id}>
      Add to collection
     </div>)}
   </div >
