0114884f76b379e2b8c20c79081fe376db816a86
grudinsky <g.rudzianskas@gmail.com>
2019-10-30 16:07:31 +0000

    remove button in dahboard works

--- src/components/albums/AlbumCard.js
+++ src/components/albums/AlbumCard.js
@@ -1,19 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+// import SoundPlayer from 'react-native-sound-player'
 
-const AlbumCard = ({ title, artist, coverImage, id, addAlbum, removeAlbum, inRekordBox }) => (
 
-  <div className="column-is-one-third album-card" value={id} key={id} >
+const AlbumCard = ({ title, artist, coverImage, id, addAlbum, removeAlbum, inRekordBox, dropDown, albumTracks }) => (
+
+  <div className='column-is-one-third album-card' value={id} key={id} >
+
     <div className="image">
       <img src={coverImage}></img>
     </div>
-    
     <div>
       <h5 className="title-size">{title}</h5>
       <h6>{`${artist.name}`}</h6>
-    </div>
-    {removeAlbum && (
-      <div onClick={(inRekordBox === false) ? addAlbum : removeAlbum} className={`flex-end button ${(inRekordBox === false) ? 'button-primary' : 'button'}`} id={id}>
-        {(inRekordBox === false) ? 'Add to collection' : 'Remove from collection'}</div>)}
+    </div >
+    <button className="button" value={id} onClick={dropDown}>
+      ▷
+    </button>
+    {albumTracks && (
+      <div>
+        {albumTracks.map(track => (
+          <div key={track.id}>
+            <p>{track.title}</p>
+            <button value={track.preview} onClick={() => track.preview.play()}>▷</button>
+            <p>{track.preview}</p>
+            {/* <button onClick={SoundPlayer.playUrl(track.preview)}>▷</button> */}
+          </div>))}
+      </div>)}
+    {
+      removeAlbum && (
+        <div onClick={(!inRekordBox) ? addAlbum : removeAlbum}
+          className={`flex-end button ${(!inRekordBox) ? 'button-primary' : 'button'}`} id={id}>
+          {(!inRekordBox) ? 'Add to collection' : 'Remove from collection'}</div>)
+    }
   </div >
 )
 
--- src/components/auth/Dashboard.js
+++ src/components/auth/Dashboard.js
@@ -11,6 +11,7 @@ class Dashboard extends React.Component {
     this.state = {
       user: null
     }
+    this.handleRemoveAlbum = this.handleRemoveAlbum.bind(this)
   }
 
   componentDidMount() {
@@ -37,6 +38,21 @@ class Dashboard extends React.Component {
       })
       .catch(err => console.log(err))
   }
+  inRekordBox(value) {
+    if (this.state.user.rekordBox) return this.state.user.rekordBox.some(record => record.deezerId === value)
+  }
+
+  handleRemoveAlbum(e) {    // creates album in DB
+    const albumId = parseInt(e.target.id)//need to parse button id as need to change data type from string to number for below filter to work
+    console.log(albumId)
+    const albumData = this.state.user.rekordBox.find(item => item.deezerId === albumId)
+
+    axios.delete(`/api/albums/${albumData._id}`, {
+      headers: { Authorization: `Bearer ${Auth.getToken()}` }
+    })
+      .then(() => this.getUser())
+      .catch(err => console.log(err))
+  }
 
   render() {
 
@@ -54,6 +70,9 @@ class Dashboard extends React.Component {
             {this.state.user.rekordBox.map(album => (
               < AlbumCard key={album.deezerId}
                 {...album}
+                removeAlbum={this.handleRemoveAlbum}
+                inRekordBox={this.inRekordBox(album.deezerId)}
+                id={album.deezerId}
               />
             ))}
           </div>
