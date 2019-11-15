2011d181eb4e7e7c8064e05c6bb3385ded842fae
Matt Wilkie <matt@pamr.co.uk>
2019-11-15 15:23:08 +0000

    test2

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 // const dbURI = 'mongodb://localhost/rekordr'
 
-const dbURI = process.env.MONGGODB_URI || `mongodb://localhost/rekordr${process.env.NODE_ENV || 'dev'}`
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/rekordr${process.env.NODE_ENV || 'dev'}`
 const port = 4000
 const secret = 'Shhhh it\'s a secret'
 
